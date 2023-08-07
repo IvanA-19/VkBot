4772a5b35adfb19de4baa161b8673245fad1d8db
Ivan Alekseev <100191862+IvanA-19@users.noreply.github.com>
2023-08-07 13:59:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import vk_api
 from vk_api.bot_longpoll import  VkBotLongPoll
 
 # Api data
-api_token = 'vk1.a.4Z4wBBpbMF81-8vdpxnTnd3dQq5s9GHmwk_KdNW529EmXDE4LjlwmBAfZTGB0oSmRHkjL_ccmIfruXCd1bTBTCeKbnA5E7Xu-5s5kMY0c9RYxKsZ7rJClyY1x3c4S3FWfIYsGMpL1324coeAidr1EM6hC0o-guP7b_YLQ9jwG2SEQmUBrPN2fiVwgNmkIs-fhdoIErn2mguWBHxaugWrAA'
-group_id = '220483099'
-group_url = 'https://vk.com/club220483099'
+api_token = 'YOUR API TOKEN'
+group_id = 'YOUR GROUP ID'
+group_url = 'YOUR GROUP URL'
 
 
 # Api and long poll
